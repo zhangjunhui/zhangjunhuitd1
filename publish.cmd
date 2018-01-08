c1996482cfda7695a16406406cc7b0e175dc7477
zhangjunhui <zhangjunhuitd@163.com>
2018-01-08 16:52:36 +0800

    change set1
    change set1

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=demo-botservice-Web-Deploy.pubxml -p:Password=onyqxZh8EZztQ18KcDnFRsCu7kxDT9dQmEmkvhphDtGAR0cSlFDMldX1iMd6
+
